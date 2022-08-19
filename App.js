cc5bb5f6a32ac8aa3b147ce0ec249fa5e78904f4
TiberiusR <tiburssiu@hotmail.com>
2022-08-19 14:36:40 -0300

    react native kanye west

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('Press Kanye for a quote');
+
+  function getQuote() {
+    return fetch("https://api.kanye.rest")
+      .then ((response) => response.json())
+      .then ((responseJson) => {
+        setText(responseJson.quote);
+      })
+      .catch ((error) => {
+        console.error(error);
+      });
+  }
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{text}</Text>
+      <TouchableOpacity style={styles.button} onPress={getQuote}>
+        <Image source={require("./assets/kanye.png")}/>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +35,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    position: 'relative',
+    fontSize: 18,
+    lineHeight: 24,
+    width: 300,
+    padding: 24
+  },
+
+  button: {
+    backgroundColor: '#fff',
+ }
 });
